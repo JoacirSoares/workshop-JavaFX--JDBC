412ed19d57999916816abf528a08ed44716ba055
JoacirSoares <joacir2002@gmail.com>
2022-02-07 14:26:00 -0300

    Adding method that implements page loading

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,7 @@
 package application;
 import java.io.IOException;import javafx.application.Application;import javafx.fxml.FXMLLoader;import javafx.scene.Scene;import javafx.scene.control.ScrollPane;import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application {		private static Scene mainScene;	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -11,7 +11,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -20,7 +20,7 @@ public class Main extends Application {
 		}
 	}
 
-	public static void main(String[] args) {
+	public static Scene getMainScene() {		return mainScene;	}	public static void main(String[] args) {
 		launch(args);
 	}
 }
