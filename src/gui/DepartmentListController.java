10dd3c18069cd7ae3b53cc753c4184293c79d486
JoacirSoares <joacir2002@gmail.com>
2022-02-07 16:02:08 -0300

    Adding DepartmentListController

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,38 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import model.entities.Department;
+
+public class DepartmentListController implements Initializable{
+	
+	@FXML
+	private TableView<Department> tableViewDepartment;
+	
+	@FXML
+	private TableColumn<Department, Integer> tableColumnId;
+	
+	@FXML
+	private TableColumn<Department, String> tableColumnName;
+	
+	@FXML
+	private Button btNew; 
+	
+	@FXML
+	public void onBtNewAction() {
+		System.out.println("onBtNewAction");
+	} 
+	
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+	}
+
+}
