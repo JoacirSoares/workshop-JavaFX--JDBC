dec94925292a6dff394e9290451e3b0ce06f1a36
JoacirSoares <joacir2002@gmail.com>
2022-02-07 15:10:22 -0300

    Adding DepartmentList view design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -33,7 +33,7 @@ public class MainViewController implements Initializable{
 	}
 	
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuItemDepartmentAction");
+		loadView("/gui/DepartmentList.fxml");
 	}
 
 	public void onMenuItemAboutAction() {
